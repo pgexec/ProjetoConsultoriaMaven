098b2c3afd4818ddf97097227c5638b1181f5796
pgexec <paulo.gomesjr123@gmail.com>
2024-11-08 12:35:40 -0300

    feat. estou adicionando a tela de listar e adicionando as funcionalidades da tela

---
+++ src/main/java/controller/controllerListar.java
@@ -0,0 +1,33 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
+public class controllerListar {
+
+    @FXML
+    private Button btexcluir;
+
+    @FXML
+    private TableColumn<?, ?> columnAltura;
+
+    @FXML
+    private TableColumn<?, ?> columnAltura1;
+
+    @FXML
+    private TableColumn<?, ?> columnAltura11;
+
+    @FXML
+    private TableColumn<?, ?> columnCpf;
+
+    @FXML
+    private TableColumn<?, ?> columnData;
+
+    @FXML
+    private TableColumn<?, ?> columnNome;
+
+    @FXML
+    private TableColumn<?, ?> columnPeso;
+
+}
